965f23d64130e088338a13d76da2f3a21064df5b
Rocky Haque <rockyhaque99@gmail.com>
2024-04-06 01:25:06 +0600

    score life point updated

--- js/app.js
+++ js/app.js
@@ -1,23 +1,43 @@
-// let playNowBtn = document.getElementById('play-now');
-
-// let home = document.getElementById('home');
-// let playGround = document.getElementById('play-ground')
-// let scoring = document.getElementById('scoring')
-
-// function playNowFunc(){
-//     home.classList.add("hidden");
-//     playGround.classList.remove("hidden");
-// }
-
-// playNowBtn.addEventListener("click", playNowFunc)
-
-function continueGame() {
-    const alphabet = getARandomAlphabet();
-    console.log(alphabet);
-}
 
 function play() {
   hideElementById("home");
   showElementById("play-ground");
   continueGame();
 }
+
+function continueGame() {
+  const alphabet = getARandomAlphabet();
+  const currentAlphabetElement = document.getElementById('current-alphabet');
+  currentAlphabetElement.innerText = alphabet
+  setBgColorById(alphabet)
+}
+
+function handleKbdBtnPress(e){
+  const playerPressed = e.key;
+  const currentAlphabetElement = document.getElementById('current-alphabet');
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = currentAlphabet.toLowerCase();
+
+  if(playerPressed === expectedAlphabet){
+    const currentScoreElement = document.getElementById('current-score');
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    const newScore = currentScore + 1;
+    currentScoreElement.innerText = newScore;
+
+    console.log(currentScore);
+
+    removeBgColorById(expectedAlphabet)
+    continueGame();
+  } else{
+    const currentLifeElement = document.getElementById('current-life');
+    const currentLifeText = currentLifeElement.innerText;
+    const currentLife = parseInt(currentLifeText);
+    const newLife = currentLife - 1;
+    currentLifeElement.innerText = newLife;
+  }
+  console.log(playerPressed ,expectedAlphabet);
+  
+}
+
+document.addEventListener('keyup', handleKbdBtnPress);
--- js/utility.js
+++ js/utility.js
@@ -8,15 +8,23 @@ function showElementById(elementId) {
   element.classList.remove("hidden");
 }
 
+function setBgColorById(elementId){
+  const element = document.getElementById(elementId);
+  element.classList.add('bg-purple-500')
+}
+
+function removeBgColorById(elementId){
+  const element = document.getElementById(elementId);
+  element.classList.remove('bg-purple-500')
+}
+
 function getARandomAlphabet() {
   const alphabetString = "abcdefghijklmnopqrstuvwxyz";
   const alphabets = alphabetString.split("");
-  // console.log(alphabets);
+
   const randomNumber = Math.random() * 25;
   const index = Math.round(randomNumber);
-  // console.log(index);
 
   const alphabet = alphabets[index];
-  // console.log(index, alphabet);
   return alphabet;
 }
