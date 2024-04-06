8ddbc91179e3cb1235f04ab67d597751f0f08954
Rocky Haque <rockyhaque99@gmail.com>
2024-04-07 02:13:57 +0600

    complete project

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
 
-function play() {
-  hideElementById("home");
-  showElementById("play-ground");
-  continueGame();
-}
 
 function continueGame() {
   const alphabet = getARandomAlphabet();
@@ -12,32 +7,66 @@ function continueGame() {
   setBgColorById(alphabet)
 }
 
+document.addEventListener('keyup', handleKbdBtnPress);
+
 function handleKbdBtnPress(e){
   const playerPressed = e.key;
   const currentAlphabetElement = document.getElementById('current-alphabet');
   const currentAlphabet = currentAlphabetElement.innerText;
   const expectedAlphabet = currentAlphabet.toLowerCase();
 
-  if(playerPressed === expectedAlphabet){
-    const currentScoreElement = document.getElementById('current-score');
-    const currentScoreText = currentScoreElement.innerText;
-    const currentScore = parseInt(currentScoreText);
-    const newScore = currentScore + 1;
-    currentScoreElement.innerText = newScore;
+  // if(playerPressed === 'Enter'){
+  //   play();
+  // }
 
-    console.log(currentScore);
+  // stop game if pressed 'Esc'
+  if(playerPressed === 'Escape'){
+    gameOver();
+  }
+
+  if(playerPressed === expectedAlphabet){
+    const currentScore = getTextElementValueById('current-score')
+    const newScore = currentScore + 1;
+    setTextElementValueById('current-score', newScore)
 
     removeBgColorById(expectedAlphabet)
     continueGame();
   } else{
-    const currentLifeElement = document.getElementById('current-life');
-    const currentLifeText = currentLifeElement.innerText;
-    const currentLife = parseInt(currentLifeText);
+    const currentLife = getTextElementValueById('current-life')
     const newLife = currentLife - 1;
-    currentLifeElement.innerText = newLife;
+    setTextElementValueById('current-life', newLife);
+
+    if(newLife === 0){
+      gameOver();
+    }
   }
   console.log(playerPressed ,expectedAlphabet);
   
 }
 
-document.addEventListener('keyup', handleKbdBtnPress);
+function play() {
+  hideElementById("home");
+  hideElementById('final-score')
+  showElementById("play-ground");
+
+  // reset score & life
+  setTextElementValueById('current-life', 5)
+  setTextElementValueById('current-score', 0)
+
+  continueGame();
+}
+
+
+
+function gameOver(){
+  hideElementById('play-ground')
+  showElementById('final-score');
+
+  // update final score
+  let gameOverScoring = getTextElementValueById('current-score')
+  setTextElementValueById('game-over-scoring', gameOverScoring)
+
+  // clear the last selected alphabet
+  const currentAlphabet = getElementTextById('current-alphabet');
+  removeBgColorById(currentAlphabet)
+}
--- js/utility.js
+++ js/utility.js
@@ -18,6 +18,24 @@ function removeBgColorById(elementId){
   element.classList.remove('bg-purple-500')
 }
 
+function getTextElementValueById(elementId){
+  const element = document.getElementById(elementId);
+  const elementValueText = element.innerText;
+  const value = parseInt(elementValueText);
+  return value;
+}
+
+function setTextElementValueById(elementId, value){
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+}
+
+function getElementTextById(elementId){
+  const element = document.getElementById(elementId);
+  const text = element.innerText;
+  return text;
+}
+
 function getARandomAlphabet() {
   const alphabetString = "abcdefghijklmnopqrstuvwxyz";
   const alphabets = alphabetString.split("");
